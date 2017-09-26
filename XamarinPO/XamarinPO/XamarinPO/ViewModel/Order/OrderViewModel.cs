2ab921454714ec8d246d6b4d69546b6425bfe69e
rogerhdz <Rogelio.Hernandez@globant.com>
2017-09-26 15:09:20 -0500

    Update UI. Adding Order view model

---
+++ XamarinPO/XamarinPO/XamarinPO/ViewModel/Order/OrderViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace XamarinPO.ViewModel.Order
+{
+    public class OrderViewModel
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime CreationDate { get; set; }
+        public DateTime DeliveryDate { get; set; }
+        public string DeliveryInformation { get; set; }
+        public int Client { get; set; }
+    }
+}
